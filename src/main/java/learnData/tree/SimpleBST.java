dd59e79eb388221e54344171aacb33d5a5f3aafe
xuyi <yi.xu@ele.me>
2019-02-23 14:43:28 +0800

    simplebst

--- src/main/java/learnData/tree/SimpleBST.java
+++ src/main/java/learnData/tree/SimpleBST.java
@@ -1,5 +1,17 @@
 package learnData.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 二分搜索树
+ *
+ * 基本操作:
+ * 1、插入元素
+ * 2、查询元素
+ * 3、删除元素
+ */
 public class SimpleBST {
 
     //根节点
@@ -8,7 +20,6 @@ public class SimpleBST {
     private boolean isEmpty;
 
     private class Node {
-
         private int data;
         private Node left;
         private Node right;
@@ -90,16 +101,16 @@ public class SimpleBST {
     }
 
     public boolean contains(int data) {
-        return contains(root,data);
+        return contains(root, data);
     }
 
-    private boolean contains(Node node,int data){
-        if (node==null) return false;
-        if (data==node.data)return true;
-        if (data<node.data){
-            return contains(node.left,data);
-        }else {
-            return contains(node.right,data);
+    private boolean contains(Node node, int data) {
+        if (node == null) return false;
+        if (data == node.data) return true;
+        if (data < node.data) {
+            return contains(node.left, data);
+        } else {
+            return contains(node.right, data);
         }
     }
 
@@ -109,8 +120,8 @@ public class SimpleBST {
 
     private Node minNode(Node node) {
 
-        if (node == null) throw new RuntimeException("不合法");
-        if (node.left == null){
+        if (node == null) throw new RuntimeException("空树");
+        if (node.left == null) {
             return node;
         } else {
             return minNode(node.left);
@@ -128,10 +139,10 @@ public class SimpleBST {
         return maxNode(root).data;
     }
 
-    private Node maxNode(Node node){
+    private Node maxNode(Node node) {
 
-        if (node == null) throw new RuntimeException("不合法");
-        if (node.right == null){
+        if (node == null) throw new RuntimeException("空树");
+        if (node.right == null) {
             return node;
         } else {
             return maxNode(node.right);
@@ -144,37 +155,102 @@ public class SimpleBST {
 //        return node;
     }
 
-    public void remove(int data) {
-        root=remove(root,data);
+    /**
+     * 删除最小元素
+     *
+     * @return
+     */
+    public int removeMin() {
+        int result = min();
+        root = removeMin(root);
+        return result;
     }
 
-    private Node remove(Node node,int data){
-
-        if (node==null) return null;
-        if (data<node.data){
-            node.left=remove(node.left,data);
-        } else if (data>node.data){
-            node.right=remove(node.right,data);
-        }else {
-            if (node.left==null&&node.right==null){
-                node=null;
-            }
-            if (node.left==null){
-                Node rightNode=node.right;
-                node.right=null;
-            }
-            if (node.right==null){
-                Node minRight=minNode(node);
-                minRight.right=node.right;
-                node.right=null;
-            }
-
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
             size--;
+            return rightNode;
         }
+        node.left = removeMin(node.left);
         return node;
     }
 
+    public int removeMax() {
+        int result = max();
+        root = removeMax(root);
+        return result;
+
+    }
+
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    public void remove(int data) {
+        root = remove(root, data);
+    }
+
+    /**
+     * 删除任意元素
+     * @param node
+     * @param data
+     * @return
+     */
+    private Node remove(Node node, int data) {
+
+        if (node == null) return null;
+        if (data<node.data){
+            node.left=remove(node.left,data);
+            return node;
+        }
+        else if (data>node.data){
+            node.right=remove(node.right,data);
+            return node;
+        }else {
+            //data==node.data
+            //待删除节点左子树为空
+            if (node.left==null){
+                Node rightNode=node.right;
+                node.right=null;
+                size--;
+                return rightNode;
+            }
+            //待删除节点右子树为空
+            if (node.right==null){
+                Node leftNode=node.left;
+                node.left=null;
+                size--;
+                return leftNode;
+            }
+            //待删除节点左右子树都不为空
+            //1、寻找出待删除节点右子树中最小节点(后继节点)/寻找出待删除节点左子树中最大节点(前驱节点)
+            //2、删除右子树的最小节点/删除左子树最大节点
+            //3、将后继节点或前驱节点替代待删除节点位置
+            Node successor=minNode(node.right);
+            successor.right=removeMin(node.right);
+            successor.left=node.left;
+            //回收node节点
+            node.left=node.right=null;
+            return successor;
+        }
+    }
+
     public static void main(String[] args) {
+
+        SimpleBST b=new SimpleBST(10);
+        b.add(5);
+        b.add(2);
+        b.add(15);
+        b.removeMin();
 
         SimpleBST bst = new SimpleBST(10);
         bst.add(5);
@@ -183,25 +259,51 @@ public class SimpleBST {
         bst.add(8);
         bst.add(13);
         bst.add(18);
+        bst.add(4);
+        bst.add(17);
 
         System.out.println(bst.getSize());
 
         bst.preOrder();
-
         System.out.println();
-        System.out.println("max:"+bst.max());
-        System.out.println("min:"+bst.min());
-
-        System.out.println(bst.contains(15));
 
         bst.remove(3);
-        bst.remove(8);
-        bst.remove(100);
-
-        bst.preOrder();
-
+        bst.middleOrder();
+        System.out.println();
+        bst.remove(5);
+        bst.middleOrder();
+        System.out.println();
+        bst.remove(15);
+        bst.middleOrder();
         System.out.println();
         System.out.println(bst.getSize());
+
+//        System.out.println();
+//        System.out.println("max:" + bst.max());
+//        System.out.println("min:" + bst.min());
+//
+//        System.out.println(bst.contains(15));
+//
+//
+//        bst.preOrder();
+//
+//        System.out.println();
+//        System.out.println(bst.getSize());
+//        System.out.println("------");
+//
+//
+//        SimpleBST simpleBST = new SimpleBST(5000);
+//        Random random = new Random();
+//        for (int i = 0; i < 100; i++) {
+//            simpleBST.add(random.nextInt(10000));
+//        }
+//
+//        System.out.println(simpleBST.getSize());
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 5; i++) {
+//            list.add(simpleBST.removeMin());
+//        }
+//        list.forEach(e -> System.out.print(e + "->"));
 
 
     }
