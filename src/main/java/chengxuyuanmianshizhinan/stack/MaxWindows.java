bd07b19fba5f224f1c715c001642381af7596df3
xuyi <yi.xu@ele.me>
2019-03-26 08:36:44 +0800

    stack

--- src/main/java/chengxuyuanmianshizhinan/stack/MaxWindows.java
+++ src/main/java/chengxuyuanmianshizhinan/stack/MaxWindows.java
@@ -1,18 +1,78 @@
 package chengxuyuanmianshizhinan.stack;
 
+import java.util.LinkedList;
+
+/**
+ * 生成窗口最大数组
+ */
 public class MaxWindows {
 
 
-    public int[] maxWindows(int[] arr,int m){
+    //复杂度为O(N*M)
+    public int[] maxWindowsWorse(int[] arr, int m) {
 
-        if (arr==null||arr.length==1)return arr;
-        if (arr.length<m)throw new IllegalArgumentException("不合法窗口大小");
-        int max=arr[0];
-        for (int i = 0; i <m ; i++) {
+        if (arr == null || arr.length == 1) return arr;
+        if (arr.length < m) throw new IllegalArgumentException("不合法窗口大小");
+        int[] result = new int[arr.length + 1 - m];
+        for (int i = 0; i < arr.length + 1 - m; i++) {
+            int max = arr[i];
+            for (int j = 0; j < m; j++) {
+                if (max < arr[i + j]) {
+                    max = arr[i + j];
+                }
+            }
+            result[i] = max;
+        }
+        return result;
+    }
+
+
+    //复杂度为O(N)
+    public int[] maxWindowsBetter(int[] arr, int m) {
+
+        if (arr == null || arr.length == 1) return arr;
+        if (arr.length < m) throw new IllegalArgumentException("不合法窗口大小");
+        int[] result = new int[arr.length + 1 - m];
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            while (!linkedList.isEmpty() && arr[linkedList.peekLast()] < arr[i]) {
+                linkedList.pollLast();
+            }
+            linkedList.addLast(i);
+            //过期下标
+            if (linkedList.peekFirst() == i - m) {
+                linkedList.pollFirst();
+            }
+            //从第m次循环开始就可以添加最大元素了.
+            if (i >= m - 1) {
+                result[index++] = arr[linkedList.peekFirst()];
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        MaxWindows maxWindows = new MaxWindows();
+        int[] max = maxWindows.maxWindowsWorse(new int[]{1, 2, 3, 8, 5, 6, 7}, 3);
+
+        for (int m : max) {
+            System.out.print(m + "-->");
+        }
+
+        System.out.println();
+        System.out.println("----");
+        int[] better = maxWindows.maxWindowsBetter(new int[]{1, 2, 3, 8, 5, 6, 7}, 3);
+
+        for (int m : better) {
+            System.out.print(m + "-->");
         }
 
 
-        return null;
     }
 
 }
---
+++ src/main/java/chengxuyuanmianshizhinan/stack/ValidChar.java
@@ -0,0 +1,52 @@
+package chengxuyuanmianshizhinan.stack;
+
+import java.util.Stack;
+
+/**
+ * 利用栈数据结构判断括号匹配
+ * 思路:
+ * 1、先将左括号入栈
+ * 2、如果遇到右括号就和栈顶元素比对看是否是一对比配括号
+ */
+public class ValidChar {
+
+    public boolean isValid(String string) {
+
+        Stack<Character> stack = new Stack<>();
+        char[] chars = string.toCharArray();
+        if (chars.length % 2 != 0) return false;
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                if (c == ')' && '(' != stack.pop()) return false;
+                if (c == ']' && '[' != stack.pop()) return false;
+                if (c == '}' && '{' != stack.pop()) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    //LeetCode有一个读者提供了这种解法,这个解法是有问题的
+    public boolean isValidV2(String string) {
+
+        String tmp = string.replace("()", "");
+        tmp.replace("[]", "");
+        tmp.replace("{}", "");
+        return tmp.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+
+        ValidChar validChar = new ValidChar();
+        System.out.println(validChar.isValid("{()}"));
+        System.out.println(validChar.isValid("{()[]{([])}"));
+        System.out.println(validChar.isValidV2("([]){}[{}]"));
+        System.out.println(validChar.isValid("([]){}[{}]"));
+
+    }
+
+}
