7dc4e622abbae5f9cd5e175087f8bc8d43a84f0d
xuyi <yi.xu@ele.me>
2019-07-04 17:41:29 +0800

    cyclenode

--- src/main/java/learn_data_structure/linkedlist/CycleDoubleLinkedList.java
+++ src/main/java/learn_data_structure/linkedlist/CycleDoubleLinkedList.java
@@ -1,18 +1,21 @@
 package learn_data_structure.linkedlist;
 
 /**
- * 双向链表
+ * 双向循环链表,其实和单向没什么太大区别
  */
-public class DoubleLinkedList {
+public class CycleDoubleLinkedList {
 
+
+    //头尾节点
     private Node head, tail;
+    //链表长度大小
     private int size;
 
     private static class Node {
 
-        public Node pre;
-        public Node next;
-        public int value;
+        private Node pre;
+        private Node next;
+        private int value;
 
         public Node(int value) {
             this.value = value;
@@ -23,7 +26,6 @@ public class DoubleLinkedList {
             this.next = next;
             this.value = value;
         }
-
     }
 
     public int getSize() {
@@ -37,56 +39,71 @@ public class DoubleLinkedList {
     public void add(int value) {
         if (isEmpty()) {
             head = tail = new Node(value);
+            head.next = tail;
+            head.pre = tail;
+            tail.next = head;
+            tail.pre = head;
         } else {
-            head = new Node(null, head, value);
+            head = new Node(tail, head, value);
+            tail.next = head;
         }
         size++;
-
     }
 
     public int remove() {
+
         if (isEmpty()) throw new IllegalStateException("空链表");
-        int remove = head.value;
-        if (getSize() == 1) {
+        int ret = head.value;
+        if (size == 1) {
             head = tail = null;
         } else {
+            tail.next = head.next;
             head = head.next;
-            head.pre = null;
+            head.pre = tail;
         }
         size--;
-        return remove;
+        return ret;
     }
+
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         Node cur = head;
-        while (cur != null) {
+        //第一种循环方式
+//        while (cur.next != head) {
+//            sb.append(cur.value + "->");
+//            cur = cur.next;
+//        }
+//        sb.append(cur.value);
+
+        //第二种循环方式
+        for (int i = 0; i < size; i++) {
             sb.append(cur.value + "->");
             cur = cur.next;
         }
+
         return sb.toString();
     }
 
+
     public static void main(String[] args) {
 
-        DoubleLinkedList list = new DoubleLinkedList();
-
+        CycleDoubleLinkedList list = new CycleDoubleLinkedList();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
-
         System.out.println(list.toString());
 
-
-        System.out.println(list.remove());
         System.out.println(list.remove());
         System.out.println(list.remove());
 
         System.out.println(list.toString());
+
 
     }
+
 
 }
--- src/main/java/learn_data_structure/linkedlist/CycleLinkedList.java
+++ src/main/java/learn_data_structure/linkedlist/CycleLinkedList.java
@@ -1,29 +1,29 @@
 package learn_data_structure.linkedlist;
 
 /**
- * 双向链表
+ * 循环链表
  */
-public class DoubleLinkedList {
+public class CycleLinkedList {
 
+
+    //头尾节点
     private Node head, tail;
+    //链表长度大小
     private int size;
 
     private static class Node {
 
-        public Node pre;
-        public Node next;
-        public int value;
+        private Node next;
+        private int value;
 
         public Node(int value) {
             this.value = value;
         }
 
-        public Node(Node pre, Node next, int value) {
-            this.pre = pre;
+        public Node(Node next, int value) {
             this.next = next;
             this.value = value;
         }
-
     }
 
     public int getSize() {
@@ -37,55 +37,67 @@ public class DoubleLinkedList {
     public void add(int value) {
         if (isEmpty()) {
             head = tail = new Node(value);
+            head.next = tail;
+            tail.next = head;
         } else {
-            head = new Node(null, head, value);
+            head = new Node(head, value);
+            tail.next = head;
         }
         size++;
-
     }
+
 
     public int remove() {
         if (isEmpty()) throw new IllegalStateException("空链表");
-        int remove = head.value;
-        if (getSize() == 1) {
+        int ret = head.value;
+        if (size == 1) {
             head = tail = null;
         } else {
+            tail.next = head.next;
             head = head.next;
-            head.pre = null;
         }
         size--;
-        return remove;
+        return ret;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         Node cur = head;
-        while (cur != null) {
+        //第一种循环方式
+        while (cur.next != head) {
             sb.append(cur.value + "->");
             cur = cur.next;
         }
+        sb.append(cur.value);
+
+        //第二种循环方式
+//        for (int i = 0; i < size; i++) {
+//            sb.append(cur.value + "->");
+//            cur = cur.next;
+//        }
+
         return sb.toString();
     }
 
     public static void main(String[] args) {
 
-        DoubleLinkedList list = new DoubleLinkedList();
-
+        CycleLinkedList list = new CycleLinkedList();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
-        list.add(5);
 
         System.out.println(list.toString());
 
 
         System.out.println(list.remove());
         System.out.println(list.remove());
-        System.out.println(list.remove());
 
         System.out.println(list.toString());
+
+        System.out.println(System.currentTimeMillis());
+
 
     }
 
